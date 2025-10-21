1b398bb6cbaf0621f5f827ded75e9532a7019f7b
Asher Lien <birdym.tw@gmail.com>
2025-10-21 13:46:02 +0800

    GET

--- controllers/File/GetFileController.ts
+++ controllers/File/GetFileController.ts
@@ -1,8 +1,11 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
+import File from '../../models/File'
 
 export const GetFileController = async (req: Req, res: Res) => {
+  const file = await File.find({ userId: req.user?.userId })
   res.status(StatusCodes.OK).json({
     msg: 'GetFileController_GET',
+    file,
   })
 }
