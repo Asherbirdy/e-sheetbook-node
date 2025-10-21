9ba126ee708cbc9aa91658f8490e161b408482e1
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-10-21 10:06:37 +0800

    refactor/clearNonUseApi(#3)

--- controllers/DevController.ts
+++ controllers/DevController.ts
@@ -1,17 +1,9 @@
 import {
   GetDevEnvController,
-  SetDevDistrictController,
-  DeleteNonVerifiedEmailAccountController,
   CheckIpController,
 } from './Dev/index'
 
 export const DevController = {
-  // ** 
   get: GetDevEnvController,
-  // **
-  setDevDistrict: SetDevDistrictController,
-  // **
-  deleteNonVerifiedEmailAccount: DeleteNonVerifiedEmailAccountController,
-  // **
   checkIp: CheckIpController,
 }
