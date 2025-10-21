45c2e1d08683d61dc49fdecd092a13ea577a96a3
Asher Lien <birdym.tw@gmail.com>
2025-10-21 11:31:33 +0800

    更新

--- controllers/File/CreateFileController.ts
+++ controllers/File/CreateFileController.ts
@@ -1,8 +1,29 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
+import { BadRequestError } from '../../errors'
+import File from '../../models/File'
 
 export const CreateFileController = async (req: Req, res: Res) => {
+  const { name } = req.body
+
+  if (!name) {
+    throw new BadRequestError('REQUIRED_NAME')
+  }
+
+  const isNameExist = await File.findOne({ name, userId: req.user?.userId })
+
+  if (isNameExist) {
+    throw new BadRequestError('FILE_NAME_EXIST')
+  }
+
+  const file = await File.create({
+    name,
+    userId: req.user?.userId
+  })
+
   res.status(StatusCodes.OK).json({
     msg: 'CreateFileController_POST',
+    name,
+    file,
   })
 }
--- routes/FileRoutes.ts
+++ routes/FileRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { FileController } from '../controllers'
 
+import { authenticateUser } from '../middleware'
+
 const router = Router()
 
-router.get('/', FileController.get)
-router.post('/', FileController.post)
-router.put('/', FileController.put)
-router.delete('/', FileController.delete)
+router.get('/', authenticateUser, FileController.get)
+router.post('/', authenticateUser, FileController.post)
+router.put('/', authenticateUser, FileController.put)
+router.delete('/', authenticateUser, FileController.delete)
 
 export default router
     
