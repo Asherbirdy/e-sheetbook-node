4a6c2ff3306add83252c84e18b710f04b5bfd742
Asher Lien <birdym.tw@gmail.com>
2025-10-21 16:05:34 +0800

    done

--- controllers/File/DeleteFileController.ts
+++ controllers/File/DeleteFileController.ts
@@ -1,8 +1,32 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
+import File from '../../models/File'
+import { BadRequestError } from '../../errors'
+import { checkPersmissionByUserId } from '../../utils'
 
 export const DeleteFileController = async (req: Req, res: Res) => {
+  const { fileId } = req.body
+
+  if (!fileId) {
+    throw new BadRequestError('REQUIRED_FILE_ID')
+  }
+
+  const isFileExist = await File.findById(fileId)
+
+  if (!isFileExist) {
+    throw new BadRequestError('FILE_NOT_FOUND')
+  }
+
+  if (!req.user?.userId) {
+    throw new BadRequestError('USER_NOT_FOUND')
+  }
+
+  checkPersmissionByUserId(req.user.userId, isFileExist.userId)
+
+  const file = await File.findByIdAndDelete(fileId)
+
   res.status(StatusCodes.OK).json({
     msg: 'DeleteFileController_DELETE',
+    deletedFile: file,
   })
 }
