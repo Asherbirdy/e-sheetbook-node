56160a3b65ad19095da323f44857cdce76d39b9f
Asher Lien <birdym.tw@gmail.com>
2025-10-22 16:50:16 +0800

    更新

--- controllers/Sheet/GetAllSheetController.ts
+++ controllers/Sheet/GetAllSheetController.ts
@@ -1,8 +1,11 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
+import Sheet from '../../models/Sheet'
 
 export const GetAllSheetController = async (req: Req, res: Res) => {
+  const sheets = await Sheet.find()
   res.status(StatusCodes.OK).json({
-    msg: 'GetAllSheetController_GET'
+    msg: 'GetAllSheetController_GET',
+    sheets
   })
 }
