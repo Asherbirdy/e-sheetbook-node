6c91210f3618e01c52866b2a1d02d11597a9b15c
Asher Lien <birdym.tw@gmail.com>
2025-10-22 17:28:49 +0800

    更新

--- controllers/Sheet/GetAllSheetController.ts
+++ controllers/Sheet/GetAllSheetController.ts
@@ -3,7 +3,7 @@ import { Req, Res } from '../../types'
 import Sheet from '../../models/Sheet'
 
 export const GetAllSheetController = async (req: Req, res: Res) => {
-  const sheets = await Sheet.find()
+  const sheets = await Sheet.find().populate('fileId')
   res.status(StatusCodes.OK).json({
     msg: 'GetAllSheetController_GET',
     sheets
