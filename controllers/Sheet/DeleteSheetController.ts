d50257f0ba528a12aa20cfccc11f2dd9a5753b9f
Asher Lien <birdym.tw@gmail.com>
2025-10-29 11:12:11 +0800

    done

--- controllers/Sheet/DeleteSheetController.ts
+++ controllers/Sheet/DeleteSheetController.ts
@@ -1,11 +1,28 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
 import Sheet from '../../models/Sheet'
+import { BadRequestError } from '../../errors'
 
 export const DeleteSheetController = async (req: Req, res: Res) => {
-  const sheets = await Sheet.find()
+  const { sheetId } = req.body
+
+  if (!sheetId) {
+    throw new BadRequestError('PLEASE_PROVIDE_SHEET_ID')
+  }
+
+  const sheetValid = await Sheet.findOne({
+    _id: sheetId,
+    userId: req?.user?.userId
+  })
+
+  if (!sheetValid) {
+    throw new BadRequestError('SHEET_NOT_FOUND_OR_NOT_AUTHORIZED')
+  }
+
+  const sheetDeleted = await Sheet.findOneAndDelete({ _id: sheetId })
+
   res.status(StatusCodes.OK).json({
     msg: 'DeleteSheetController_DELETE',
-    sheets
+    sheetDeleted
   })
 }
