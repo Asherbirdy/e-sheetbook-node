28fa3f8661f6a7c24e8478adbb275d6337e32ea3
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-10-30 16:34:15 +0800

    feat/GetAllSheetController更改 (#8)

--- controllers/Sheet/GetAllSheetController.ts
+++ controllers/Sheet/GetAllSheetController.ts
@@ -1,14 +1,47 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
 import Sheet from '../../models/Sheet'
+import File from '../../models/File'
 
 export const GetAllSheetController = async (req: Req, res: Res) => {
-  const sheets = await Sheet.find({ 
+  // 先查詢所有 files
+  const allFiles = await File.find({
+    userId: req?.user?.userId
+  })
+
+  // 初始化 filesMap，包含所有 files（即使沒有 sheets）
+  const filesMap = new Map()
+  allFiles.forEach((file) => {
+    filesMap.set(file._id.toString(), {
+      _id: file._id,
+      name: file.name,
+      userId: file.userId,
+      sheets: []
+    })
+  })
+
+  // 查詢所有 sheets
+  const sheets = await Sheet.find({
     userId: req?.user?.userId
   }).populate('fileId')
-  
+
+  // 將 sheets 加入對應的 file
+  sheets.forEach((sheet) => {
+    const file = sheet.fileId as any
+    const fileId = file._id.toString()
+
+    if (filesMap.has(fileId)) {
+      // 移除 fileId 欄位避免重複
+      const { fileId: _fileId, ... sheetWithoutFileId } = sheet.toObject()
+      filesMap.get(fileId).sheets.push(sheetWithoutFileId)
+    }
+  })
+
+  // 將 Map 轉換為陣列
+  const files = Array.from(filesMap.values())
+
   res.status(StatusCodes.OK).json({
     msg: 'GetAllSheetController_GET',
-    sheets
+    files
   })
 }
