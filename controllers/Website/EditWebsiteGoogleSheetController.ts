a7aa81b8ac8364c4ba0a5626b2b0a51f4836f3f7
Asher Lien <birdym.tw@gmail.com>
2025-12-05 16:59:17 +0800

    更新

--- controllers/Website/EditWebsiteGoogleSheetController.ts
+++ controllers/Website/EditWebsiteGoogleSheetController.ts
@@ -1,12 +1,42 @@
-import { BadRequestError } from '../../errors'
+import { BadRequestError, NotFoundError } from '../../errors'
 import { Req, Res } from '../../types'
+import { StatusCodes } from '../../enums'
+import Website from '../../models/Website'
 
 export const EditWebsiteGoogleSheetController = async (req: Req, res: Res) => {
-  const { websiteId, googleSheetName, googleSheetApiUrl, googleSheetId } = req.body
+  const { websiteId, googleSheetName, googleSheetApiUrl, googleSheetId, googleSheetStartRow, googleSheetFields } = req.body
 
-  if (!websiteId || !googleSheetName || !googleSheetApiUrl || !googleSheetId) {
+  if (
+    !websiteId ||
+    !googleSheetApiUrl ||
+    !googleSheetId ||
+    !googleSheetStartRow ||
+    !googleSheetFields ||
+    !googleSheetName
+  ) {
     throw new BadRequestError('ALL_FIELDS_REQUIRED')
   }
 
-  res.json({ message: 'Edit website Google sheet endpoint' })
+  // 查找網站並確認所有權
+  const website = await Website.findOne({
+    _id: websiteId,
+    user: req.user?.userId
+  })
+
+  if (!website) {
+    throw new NotFoundError('WEBSITE_NOT_FOUND')
+  }
+
+  // 更新 Google Sheet 配置
+  website.googleSheetName = googleSheetName
+  website.googleSheetApiUrl = googleSheetApiUrl
+  website.googleSheetId = googleSheetId
+  website.googleSheetStartRow = googleSheetStartRow
+  website.googleSheetFields = googleSheetFields
+  await website.save()
+
+  res.status(StatusCodes.OK).json({
+    message: 'Website Google Sheet updated successfully',
+    website
+  })
 }
