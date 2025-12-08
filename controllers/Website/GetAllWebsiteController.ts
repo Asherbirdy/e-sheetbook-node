a3cfced729354bb917f29ea2815091d326b5d416
Asher Lien <birdym.tw@gmail.com>
2025-12-08 17:07:17 +0800

    更新

--- controllers/Website/GetAllWebsiteController.ts
+++ controllers/Website/GetAllWebsiteController.ts
@@ -3,8 +3,13 @@ import { Req, Res } from '../../types'
 import Website from '../../models/Website'
 
 export const GetAllWebsiteController = async (req: Req, res: Res) => {
+  
+  const websites = await Website.find({ 
+    user: req.user?.userId 
+  }).select('-html -password')
+  
   res.status(StatusCodes.OK).json({
     message: 'Get all websites successfully',
-    websites: await Website.find({ user: req.user?.userId })
+    websites
   })
 }
