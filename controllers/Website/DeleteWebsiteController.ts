d1a2ba677e1e8696d8efbbd340be01a47c36ff71
Asher Lien <birdym.tw@gmail.com>
2025-12-08 16:20:49 +0800

    delete

--- controllers/Website/DeleteWebsiteController.ts
+++ controllers/Website/DeleteWebsiteController.ts
@@ -1,11 +1,30 @@
-import { StatusCodes } from '../../enums'
+import { BadRequestError, NotFoundError } from '../../errors'
 import { Req, Res } from '../../types'
+import { StatusCodes } from '../../enums'
 import Website from '../../models/Website'
-import { BadRequestError } from '../../errors'
 
 export const DeleteWebsiteController = async (req: Req, res: Res) => {
+  const { websiteId } = req.body
+
+  if (!websiteId) {
+    throw new BadRequestError('WEBSITE_ID_REQUIRED')
+  }
+
+  // 查找網站並確認所有權
+  const website = await Website.findOne({
+    _id: websiteId,
+    user: req.user?.userId
+  })
+
+  if (!website) {
+    throw new NotFoundError('WEBSITE_NOT_FOUND')
+  }
+
+  // 刪除網站
+  await Website.deleteOne({ _id: websiteId })
 
   res.status(StatusCodes.OK).json({
-    message: 'Website deleted successfully'
+    message: 'Website deleted successfully',
+    websiteId
   })
 }
